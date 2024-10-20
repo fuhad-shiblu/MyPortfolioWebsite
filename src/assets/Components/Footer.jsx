ec1c419068a8b486e08cd4ff8fc0c3f08abdf439
Fuhad Hasan Shiblu <145831086+fuhad-shiblu@users.noreply.github.com>
2024-10-20 11:10:09 +0600

    fixed bugs footer credit

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <footer className="text-xl text-center mt-[50px] text-white font-light mb-[100px]">&#169;2024, <Link className="font-bold hover:underline" to={'/'}>Fuhad Hasan Shiblu</Link> | All Rights Reserved</footer>
-      <div className="w-full h-1"></div>
+      <div className="w-full h-1 block md:hidden"></div>
     </>
   )
 }
